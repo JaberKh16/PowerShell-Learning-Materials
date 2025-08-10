237061703ec43dfee7ae9b5ce66f72a59ee35c6b
Jaber Khan <md.jaberkhan66@gmail.com>
2025-08-10 23:53:09 +0600

    comparision-operators-concepts

---
+++ powershell-scripts/8.comparsion-operation.ps1
@@ -0,0 +1,12 @@
+# comparsion operators in powershell
+# available operators are following:
+# -eq   => equal to
+# -ne   => not equal to-
+# -gt   => greater than
+# -ge   => greater than equal to
+# -lt   => less than
+# -le   => less than equal to
+
+2 -eq 3
+2 -ne 3
+
