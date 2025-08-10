59f25bbd7131727bfd8a865a4d2921c15e00e02d
Jaber Khan <md.jaberkhan66@gmail.com>
2025-08-10 23:52:46 +0600

    arithmatic-operators-concepts

---
+++ powershell-scripts/7.arithmetic-operators.ps1
@@ -0,0 +1,15 @@
+$num1 = 4
+$num2 = 5
+
+$add_result  = $num1 + $num2
+$sub_result  = $num1 - $num2
+$mul_result  = $num1 * $num2
+$divi_result = $num1 / $num2
+$mod_result  = $num1 % $num2
+
+$add_result
+$sub_result
+$mul_result
+$divi_result
+$mod_result
+
