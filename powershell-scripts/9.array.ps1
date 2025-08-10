8d70f63495883f00920f3747d7a0e1be1a0022d0
Jaber Khan <md.jaberkhan66@gmail.com>
2025-08-10 23:53:29 +0600

    array-concepts

---
+++ powershell-scripts/9.array.ps1
@@ -0,0 +1,24 @@
+
+# define array
+$numbers = 1, 2, 3, 4, 5
+$numbers
+
+# get type
+$numbers.GetType()
+
+# get the count
+$numbers.Count
+
+# get 1st index value
+$numbers[0]
+
+# get range
+$nubers[0 .. 3]
+
+
+# define range array
+$range_numbers = 2 .. 20
+$range_numbers
+
+# reverse order range
+$numbers[-3 .. 0]
