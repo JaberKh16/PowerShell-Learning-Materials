a676fc8c8c5542f90cd519e012155c1ec7ef1cd8
Jaber Khan <md.jaberkhan66@gmail.com>
2025-08-23 07:51:22 +0600

    function-concepts

--- powershell-scripts/19.function.ps1
+++ powershell-scripts/19.function.ps1
@@ -3,29 +3,18 @@ function Get-CurrentDateTime {
     return (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
 }
 
-# Function to get system information
-function Get-SystemInformation {
+# Function to get ping count
+function Get-PingCount {
     param (
-        [string]$reportFile = "./system-info-report.txt"
+        [string]$Target = "google.com"
     )
-    # Initialize variable
-    $reportData = ""
-    $reportData += "`n--- System Information Report ---`n"
-    $reportData += "Report generated on: $(Get-CurrentDateTime)`n"
-    $reportData += "----------------------------------`n"
-    $reportData += "`n--- CPU Information ---`n"
-    $reportData += (Get-CimInstance -ClassName Win32_Processor | Out-String)
-    $reportData += "`n--- Memory Information ---`n"
-    $totalMemory = [System::Math]::Round((Get-CimInstance -ClassName Win32_OperatingSystem).TotalVisibleMemorySize / 1MB, 2)
-    $freeMemory  = [System::Math]::Round((Get-CimInstance -ClassName Win32_OperatingSystem).FreePhysicalMemory / 1MB, 2)
-    $usedMemoryPercentage = [math]::Round(100 - (($freeMemory / $totalMemory) * 100), 2)
-    $freeMemoryPercentage = [math]::Round(($freeMemory / $totalMemory) * 100, 2)
-    $reportData += "`nUsed Memory in %  : $usedMemoryPercentage"
-    $reportData += "`nFree Memory in %  : $freeMemoryPercentage"
-    $reportData += "`n--- Disk Information ---`n"
-    $reportData += (Get-PSDrive -PSProvider FileSystem | Select-Object Name, @{Name="Used(GB)"; Expression={[math]::Round($_.Used / 1GB, 2)}}, @{Name="Free(GB)"; Expression={[math]
-
+    $pingCount = Read-Host "Enter number of ping attempts (default 4)"
+    if (-not $pingCount) {
+        $pingCount = 4
+    }
+    return $pingCount
 }
 
-# call the function
-Get-SystemInformation -reportFile "./system-info-report.txt"
+# call the functions
+$currentDateTime = Get-CurrentDateTime
+$pingCount = Get-PingCount
