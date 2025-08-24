97eb08df79003a85e352dc0ec1d7d93af33f406b
Jaber Khan <md.jaberkhan66@gmail.com>
2025-08-24 23:56:18 +0600

    compressed-file-and-folder

---
+++ powershell-scripts/practices/12.compressed-file-or-folder.ps1
@@ -0,0 +1,6 @@
+
+# compresed work
+Compressed-Archive -Path "C:\NewFolder\File.txt" -DestinationPath "C:\NewFolder\file.zip" # compressed a file
+
+
+Compressed-Archive -Path "C:\NewFolder"  -DestinationPath "C:\NewFolder.zip" # compressed a folder
