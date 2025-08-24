fdfe28dbbfd919c900ffa734849064d656afb82b
Jaber Khan <md.jaberkhan66@gmail.com>
2025-08-24 23:56:40 +0600

    unzip-file-and-folder

---
+++ powershell-scripts/practices/13.unzip-file-or-folder.ps1
@@ -0,0 +1,11 @@
+
+# unzip work
+Expand-Archive -Path "C:\NewFolder\File.zip" -DestinationPath "C:\NewFolder\file.txt" # unzip a file
+
+
+Expand-Archive -Path "C:\NewFolder.zip"  -DestinationPath "C:\NewFolder" # unzip a folder
+
+
+Expand-Archive -Path "C:\NewFolder.zip" -DestinationPath "C:\NewFolder" -Force # to unzip with force while having similar file/directory
+
+
