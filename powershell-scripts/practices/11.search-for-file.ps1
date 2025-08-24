043f315a61ffa75fac9c7075cc57b999997e5406
Jaber Khan <md.jaberkhan66@gmail.com>
2025-08-24 23:44:58 +0600

    search-for-file

---
+++ powershell-scripts/practices/11.search-for-file.ps1
@@ -0,0 +1,5 @@
+# search for file 
+Get-ChildItem -Path "C:\NewFolder" -Filter "*.txt"  # to search for .txt files in current directory
+
+Get-ChildItem -Path "C:\NewFolder" -Filter "*.txt" -Recurse # to search for all directories .txt file
+
