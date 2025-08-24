0ddf8b8c023c43653057e313330c1fba29ac08f8
Jaber Khan <md.jaberkhan66@gmail.com>
2025-08-25 00:16:55 +0600

    networking-commands-scripts

---
+++ powershell-scripts/practices/16.networking-Commands.ps1
@@ -0,0 +1,6 @@
+# Basic Networking Commands
+
+Get-NewIPAdress      # to know about ip address
+Get-NetworkAdapter   # to know about network adapter
+
+Test-Connection -ComputerName google.com -count 4 # with 4 ping
